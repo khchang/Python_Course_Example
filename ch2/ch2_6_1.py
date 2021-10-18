f9d40eb7a277507138f7d14b0e6e8e76174dc262
kh <ee.khchang@gmail.com>
2021-10-18 14:35:30 +0800

    Modify

--- ch2/ch2_6_1.py
+++ ch2/ch2_6_1.py
@@ -1,10 +1,11 @@
 # ch2_6_1.py
 
-PI = 3.14159
-r = 5
+PI = 3.14159    # 圓周率
+r = 5           # 半徑
 print("圓面積:單位是平方公分")
 area = PI * r * r
 print(area)
+
 length = 2 * PI * r
 print("圓周長:單位是公分")
 print(length)
--- ch3/ch3_10.py
+++ ch3/ch3_10.py
@@ -3,32 +3,41 @@ x = -10
 print("以下輸出abs( )函數的應用")
 print(x)            # 輸出x變數
 print(abs(x))       # 輸出abs(x)
+print("-----------------------")
 x = 5
 y = 3
 print("以下輸出pow( )函數的應用")
 print(pow(x, y))    # 輸出pow(x,y)
+print("-----------------------")
 x = 47.5
 print("以下輸出round(x)函數的應用")
 print(x)            # 輸出x變數
 print(round(x))     # 輸出round(x)
+print("-----------------------")
 x = 48.5
 print(x)            # 輸出x變數
 print(round(x))     # 輸出round(x)
+print("-----------------------")
 x = 49.5
 print(x)            # 輸出x變數
 print(round(x))     # 輸出round(x)
+print("-----------------------")
 print("以下輸出round(x,n)函數的應用")
 x = 2.15
 print(x)            # 輸出x變數
 print(round(x,1))   # 輸出round(x,1)
+print("-----------------------")
 x = 2.25
 print(x)            # 輸出x變數
 print(round(x,1))   # 輸出round(x,1)
+print("-----------------------")
 x = 2.151
 print(x)            # 輸出x變數
 print(round(x,1))   # 輸出round(x,1)
+print("-----------------------")
 x = 2.251
 print(x)            # 輸出x變數
 print(round(x,1))   # 輸出round(x,1)
+print("-----------------------")
 
 
