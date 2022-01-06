5bbe632295acc1e398fe00402e39c9c546fb1ba1
kh <ee.khchang@gmail.com>
2022-01-06 11:48:07 +0800

    Update

--- ch40/ch40_2.py
+++ ch40/ch40_2.py
@@ -3,6 +3,39 @@ import requests, os
 """
 發送 Line Notify 訊息與圖片
 """
+token = "YskG1vGeWISsQdlYyOjyeRObmmBxUhSmU28MmJTDf0L"   # 個人
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def lineNotify(token, msg, picURI):
     url = "https://notify-api.line.me/api/notify"
     headers = {
@@ -14,9 +47,20 @@ def lineNotify(token, msg, picURI):
     r = requests.post(url, headers = headers, params = payload, files = files)
     return r.status_code
  
- 
-token = "YskG1vGeWISsQdlYyOjyeRObmmBxUhSmU28MmJTDf0L"   # 個人
-msg = "\n國立虎尾科技大學\n電機工程系\n系統控制研究室\nLine傳訊傳圖測試"
-picURI = "photo_1.jpg"
+
+
+# msg = "\n國立虎尾科技大學\n電機工程系\n系統控制研究室\nLine傳訊傳圖測試"
+msg = "\nHello World! NFU"
+picURI = "D:\\Course_Region\\Python_Course_Example\\ch40\\photo_1.jpg"
  
 lineNotify(token, msg, picURI)
+
+
+
+
+
+
+
+
+
+
